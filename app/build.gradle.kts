f1ade9e369281bcf8ecb12ea14b3a237afedcc3d
TaphT <109799893+TaphT@users.noreply.github.com>
2025-03-07 14:45:28 +0700

    Fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.play.services.maps)gi
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
